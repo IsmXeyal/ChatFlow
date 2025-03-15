90b59b70b5ee7527d8bc1437ea067e506525333a
Xeyal Ism <Asus@LAPTOP-PJ5F6UB5>
2025-03-15 22:44:04 +0400

    Added Docker Compose configuration for chatflow services

---
+++ Presentation/ChatFlow.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ChatFlow.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
